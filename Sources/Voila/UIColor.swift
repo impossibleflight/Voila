80bef7a6fb79a12f4ea8928b42de4ca77ae4cd4a
John Clayton <john@fivesquare.net>
2020-01-02 12:13:31 -0500

    Added some more static colors

--- Sources/Voila/UIColor.swift
+++ Sources/Voila/UIColor.swift
@@ -10,6 +10,30 @@ import UIKit
 
 //TODO: Use Sourcery to generate these
 
+//MARK: White
+
+public extension UIColor {
+	static var white55: UIColor {
+		return UIColor(white: 0.55, alpha: 1)
+	}
+	static var white65: UIColor {
+		return UIColor(white: 0.65, alpha: 1)
+	}
+	static var white75: UIColor {
+		return UIColor(white: 0.75, alpha: 1)
+	}
+	static var white85: UIColor {
+		return UIColor(white: 0.85, alpha: 1)
+	}
+	static var white95: UIColor {
+		return UIColor(white: 0.95, alpha: 1)
+	}
+	static func white(_ white: CGFloat) -> UIColor {
+		return UIColor(white: white, alpha: 1.0)
+	}
+}
+
+
 //MARK: Translucent white
 
 public extension UIColor {
@@ -29,6 +53,30 @@ public extension UIColor {
 		return UIColor(white: 1, alpha: 0.95)
 	}
 }
+
+//MARK: Black
+
+public extension UIColor {
+	static var black55: UIColor {
+		return UIColor(white: 1-0.55, alpha: 1)
+	}
+	static var black65: UIColor {
+		return UIColor(white: 1-0.65, alpha: 1)
+	}
+	static var black75: UIColor {
+		return UIColor(white: 1-0.75, alpha: 1)
+	}
+	static var black85: UIColor {
+		return UIColor(white: 1-0.85, alpha: 1)
+	}
+	static var black95: UIColor {
+		return UIColor(white: 1-0.95, alpha: 1)
+	}
+	static func black(_ white: CGFloat) -> UIColor {
+		return UIColor(white: 1-white, alpha: 1.0)
+	}
+}
+
 
 //MARK: Grayscale
 
